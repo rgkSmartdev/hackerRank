93b499bc38a2b376d8f42f366bd0d38281101937
smartdev <rgk.smartdev@outlook.com>
2025-07-02 07:20:30 +0530

    1d array solution

--- src/main/java/org/smartdev/onedarray/Solution.java
+++ src/main/java/org/smartdev/onedarray/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
         Scanner scan = new Scanner(System.in);
 
         int q = scan.nextInt();
